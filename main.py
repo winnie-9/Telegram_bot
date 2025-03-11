00a31c65eb6e14d512b284ab49d649ec8a29c101
shalom-cyberlab <oh.shalom.0@gmail.com>
2025-03-11 15:54:54 +0000

    add .env file

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+import os
 from typing import Final
 from telegram import Update
-from telegram.ext import Application, CommandHandler,MessageHandler,filters,ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from dotenv import load_dotenv
 
-TOKEN:Final = "7501882405:AAG-qIuKKuz5aYG0yWbOzWO7n6Dq3azuSu8"
+load_dotenv()
+API_KEY: Final = os.getenv("API_KEY")
+BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
+
+
+TOKEN: Final = API_KEY
 BOT_USERNAME: Final = "@movewithme"
 
-#commands
-async def start_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
+# commands
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello. I am squishy")
 
-async def help_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("I am a music bot. Just gimme the title of any song or the most popular line in the song and I will get it for you  ")
 
-async def custom_command(update:Update,context:ContextTypes.DEFAULT_TYPE):
+
+async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("This is a custom command")
 
-#Handle res ponses
+# Handle res ponses
 
-def handle_response(text: str) ->str:
+
+def handle_response(text: str) -> str:
 
     processed: str = text.lower()
     if "hello" in processed:
@@ -36,21 +48,17 @@ def handle_response(text: str) ->str:
         return "oh okay,cool"
     if "good" in processed:
         return "cool"
-    
+
     return "i don't understand"
 
-async def handle_message(update: Update,context:ContextTypes.DEFAULT_TYPE ):
-    message_type: str = update.message.chat.type
-    text:str = update.message.text
 
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
 
     print(f'User({update.message.chat.id}) in {message_type}:"{text}"')
 
-    if message_type=="group":
+    if message_type == "group":
         if BOT_USERNAME in text:
-            new_text:str=text.replace(BOT_USERNAME, '').strip()
-            response:str=handle_response(new_text)
-
-
-    
-    
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
